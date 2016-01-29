911de7628cd8f0c3ea005e4a912c3d2fefa2b1d8
andy hunt <andyhunt200@hotmail.com>
2016-01-29 13:44:30 +0000

    event-listeners on each box

--- js/app.js
+++ js/app.js
@@ -15,25 +15,30 @@
 // }
 $ (function(){
 
-var row1 = [1,2,3,4];
-var row2 = [5,6,7,8];
-var row3 = [9,10,11,12];
-var row4 = [13,14,15,16];
+var row1 = ['1','2','3','4'];
+var row2 = ['5','6','7','8'];
+var row3 = ['9','10','11','12'];
+var row4 = ['13','14','15','16'];
 var grid = [row1, row2, row3, row4];
 
-$("1").click(function(){
-  console.log("You clicked box 1")
-});
+$('.box').on('click', function() {
+  console.log("You clicked box " + $(this).index());
+})
 
-var elements = $('.grid').children();
+// $(row1['1']).click(function(){
+//   console.log("You clicked box 1")
+// });
 
-// console.log(elements)
+// var elements = $('.grid').children();
 
-var row1 = elements.splice(0,4);
-var row2 = elements.splice(0,4);
-var row3 = elements.splice(0,4);
-var row4 = elements.splice(0,4);
-console.log(row1, row2, row3, row4)
+// // console.log(elements)
+
+// var row1 = elements.splice(0,4);
+// var row2 = elements.splice(0,4);
+// var row3 = elements.splice(0,4);
+// var row4 = elements.splice(0,4);
+// console.log(row1, row2, row3, row4)
+
 
 
 
